dc7435fd6b82bdd540f68ac564df4116466fc37b
pharma999 <pharma999999999@gmail.com>
2025-09-20 21:41:12 +0530

    Update wait-for-db.sh

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 set -e
 
-# Default values if not provided
+# Default values if env variables are missing
 DB_HOST=${DB_HOST:-db}
 DB_PORT=${DB_PORT:-5432}
 
-echo "Waiting for database at $DB_HOST:$DB_PORT ..."
+echo "Waiting for database at $DB_HOST:$DB_PORT..."
 
-# Wait until DB is ready
-until nc -z "$DB_HOST" "$DB_PORT"; do
+# Loop until database port is open
+while ! nc -z "$DB_HOST" "$DB_PORT"; do
   echo "Database not ready - waiting..."
   sleep 2
 done
 
-echo "Database is up! Starting the app..."
+echo "Database is up - starting app!"
 exec ./main
