749f38e500c8d847b6778a6f7fb6535ce8eb111d
pharma999 <pharma999999999@gmail.com>
2025-09-21 16:54:48 +0530

    Update wait-for-db.sh

--- wait-for-db.sh
+++ wait-for-db.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
-# wait-for-db.sh
-
 set -e
 
 host="pharma-db"
@@ -9,18 +6,14 @@ port=5432
 
 echo "⏳ Waiting for database to be ready at $host:$port..."
 
-# Wait until PostgreSQL is ready
 while ! nc -z "$host" "$port"; do
   echo "Database not ready - waiting..."
   sleep 2
 done
 
 echo "✅ Database is up!"
-
-# Debug: Print the DB_CONN string
 echo "🔎 DB_CONN string:"
 echo "$DB_CONN"
 
-# Start the app
-echo "🚀 Starting the app..."
-exec sh -c "./main"
+# Start the app using absolute path
+exec /app/main
